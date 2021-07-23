c7c40f7dfa88714f16680bfcb3c37de5412e6f80
Aaron Edmistone <aaron.edmistone@gmail.com>
2021-07-24 02:46:55 +1000

    Include builds

--- ok.cmd/IOkPlugin.cs
+++ ok.cmd/IOkPlugin.cs
@@ -6,51 +6,6 @@ using System.Threading.Tasks;
 
 namespace ok.cmd
 {
-    //public abstract class CmdBase
-    //{
-    //    /// <summary>
-    //    /// The command name, this is not case-sensitive and cannot have spaces
-    //    /// </summary>
-    //    public virtual string Command => null;
-    //    /// <summary>
-    //    /// A short description of the command, limited to 100 characters.
-    //    /// Any extra characters will be truncated.
-    //    /// </summary>
-    //    public virtual string Description => null;
-    //    /// <summary>
-    //    /// The syntax for the command e.g.
-    //    /// exampleCmd firstArg secondArg (optionalThirdArg) (optionalFourthArg)
-    //    /// </summary>
-    //    public virtual string Syntax => null;
-    //    /// <summary>
-    //    /// The version number, used to determine the best binary to load for this command.
-    //    /// </summary>
-    //    public virtual double Version => 0.0;
-    //    /// <summary>
-    //    /// Whether the console should pause at the end or just close.
-    //    /// </summary>
-    //    public virtual bool PauseOnComplete => false;
-    //    /// <summary>
-    //    /// If true, runs from the source directory rather than running from the current directory.
-    //    /// </summary>
-    //    public virtual bool RunFromSourceDirectory => false;
-    //    /// <summary>
-    //    /// The directory of the plugin .DLL, this is automatically assigned before the plugin is executed.
-    //    /// Useful for referencing files relative to the .DLL location.
-    //    /// </summary>
-    //    public string SourceDirectory { get; set; }
-
-    //    /// <summary>
-    //    /// Executes the command (place your code here) and returns true if the arguments were valid.
-    //    /// </summary>
-    //    /// <param name="args">The arguments for the command</param>
-    //    /// <returns>true if the args were valid for the command, otherwise false.</returns>
-    //    public virtual bool Execute(params string[] args)
-    //    {
-    //        return false;
-    //    }
-    //}
-
     public interface IOkPlugin
     {
         /// <summary>
